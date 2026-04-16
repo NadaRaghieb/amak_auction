90723d10d02faa5d0de0152ae2e74c6a6c1e5d44
Nada AlRaghieb <nraghieb@amak.local>
2026-04-16 11:33:28 +0300

    fixed job file

--- server/jobs/checkEndedAuctions.js
+++ server/jobs/checkEndedAuctions.js
@@ -1,67 +1,67 @@
-// const Auction = require("../models/Auction");
-// const Bid = require("../models/Bid");
-// const {
-//   sendAuctionEndedEmail,
-//   sendWinnerEmail,
-// } = require("../services/emailService");
+const Auction = require("../models/Auction");
+const Bid = require("../models/Bid");
+const {
+  sendAuctionEndedEmail,
+  sendWinnerEmail,
+} = require("../services/emailService");
 
-// const checkEndedAuctions = async () => {
-//   try {
-//     const now = new Date();
+const checkEndedAuctions = async () => {
+  try {
+    const now = new Date();
 
-//     const endedAuctions = await Auction.find({
-//       auctionEnd: { $lte: now },
-//       finalized: false,
-//     });
+    const endedAuctions = await Auction.find({
+      auctionEnd: { $lte: now },
+      finalized: false,
+    });
 
-//     for (const auction of endedAuctions) {
-//       const highestBid = await Bid.findOne({ auctionId: auction._id })
-//         .populate("userId", "name email")
-//         .sort({ bidAmount: -1 });
+    for (const auction of endedAuctions) {
+      const highestBid = await Bid.findOne({ auctionId: auction._id })
+        .populate("userId", "name email")
+        .sort({ bidAmount: -1 });
 
-//       if (highestBid) {
-//         await Bid.updateMany(
-//           { auctionId: auction._id, _id: { $ne: highestBid._id } },
-//           { status: "lost" }
-//         );
+      if (highestBid) {
+        await Bid.updateMany(
+          { auctionId: auction._id, _id: { $ne: highestBid._id } },
+          { status: "lost" }
+        );
 
-//         highestBid.status = "winner";
-//         await highestBid.save();
+        highestBid.status = "winner";
+        await highestBid.save();
 
-//         auction.winnerBidId = highestBid._id;
-//         auction.winnerUserId = highestBid.userId?._id || null;
-//         auction.winningAmount = highestBid.bidAmount;
-//       }
+        auction.winnerBidId = highestBid._id;
+        auction.winnerUserId = highestBid.userId?._id || null;
+        auction.winningAmount = highestBid.bidAmount;
+      }
 
-//       auction.finalized = true;
-//       auction.closedAt = now;
-//       auction.status = "ended";
+      auction.finalized = true;
+      auction.closedAt = now;
+      auction.status = "ended";
 
-//       if (!auction.resultEmailSent) {
-//         await sendAuctionEndedEmail({
-//           auctionTitle: auction.title_en,
-//           highestBidAmount: highestBid?.bidAmount || null,
-//           highestBidderName: highestBid?.userId?.name || null,
-//           highestBidderEmail: highestBid?.userId?.email || null,
-//         });
+      if (!auction.resultEmailSent) {
+        await sendAuctionEndedEmail({
+          auctionTitle: auction.title_en,
+          highestBidAmount: highestBid?.bidAmount || null,
+          highestBidderName: highestBid?.userId?.name || null,
+          highestBidderEmail: highestBid?.userId?.email || null,
+        });
 
-//         if (highestBid?.userId?.email) {
-//           await sendWinnerEmail({
-//             email: highestBid.userId.email,
-//             name: highestBid.userId.name,
-//             auctionTitle: auction.title_en,
-//             winningAmount: highestBid.bidAmount,
-//           });
-//         }
+        if (highestBid?.userId?.email) {
+          await sendWinnerEmail({
+            email: highestBid.userId.email,
+            name: highestBid.userId.name,
+            auctionTitle: auction.title_en,
+            winningAmount: highestBid.bidAmount,
+          });
+        }
 
-//         auction.resultEmailSent = true;
-//       }
+        auction.resultEmailSent = true;
+      }
 
-//       await auction.save();
-//     }
-//   } catch (error) {
-//     console.error("checkEndedAuctions error:", error.message);
-//   }
-// };
+      await auction.save();
+    }
+  } catch (error) {
+    console.error("checkEndedAuctions error:", error.message);
+  }
+};
 
-// module.exports = checkEndedAuctions;
+module.exports = checkEndedAuctions;
